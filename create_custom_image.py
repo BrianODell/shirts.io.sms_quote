6e69e8f7a5da2901df925ceb14c89a9d4e4eb9ac
Brian O'Dell <briantodell@gmail.com>
2014-03-08 10:33:26 -0500

    text is centered on tshirt

--- create_custom_image.py
+++ create_custom_image.py
@@ -1,16 +1,31 @@
+import textwrap
+
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw 
 
 def put_message_on_shirt(msg, f):
-    x = 10
-    y = 10
+    x_offset = 125
+    y_rows = [125, 160, 195, 230, 265]  # y coordinate for each line
+    width = 350
+    max_line_length = 11 # number of characters on a line
     text_color = (0,0,0)
-    font = ImageFont.truetype("libresansserifssk.ttf", 16)
 
+    #font = ImageFont.truetype("libresansserifssk.ttf", 40)
+    font = ImageFont.truetype("SourceCodePro-Bold.otf", 36)
     img = Image.open(f)
     draw = ImageDraw.Draw(img)
-    draw.text((x, y), msg , text_color,font=font)
+
+    lines = textwrap.wrap(msg, max_line_length)
+    for row, line in enumerate(lines):
+        if row < len(y_rows):
+            y = y_rows[row]
+            w, h = draw.textsize(line, font)
+            _x = x_offset + (width-w)/2
+            draw.text((_x, y), line, text_color, font=font)
+        else:
+            print "Message too long, not printing: '%s'" % (line)
+
     img.save('sample-out.jpg')
 
 
@@ -19,10 +34,11 @@ if __name__ == '__main__':
     import requests
     from StringIO import StringIO
     from run import get_tshirt_image_url
+    from sys import argv
 
     image_url = get_tshirt_image_url('Red')
     r = requests.get(image_url)
     f = StringIO(r.content)
     f.seek(0)
-    put_message_on_shirt("Test", f)
+    put_message_on_shirt(argv[1], f)
 
